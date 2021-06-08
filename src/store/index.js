ba128c8be99f702aef23e4e9c7885fdf4339e982
Miracle@pro <luomengfanchen@outlook.com>
2021-06-08 14:15:54 +0800

    添加了vuex管理数据

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,42 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    modules: {}
+    state: {
+        user: '',
+        friendList: [],
+        chatList: []
+    },
+    getters: {
+        getUser: (state) => {
+            return state.user
+        },
+        getFriendlist: (state) => {
+            return state.friendList
+        },
+        getChatlist: (state) => {
+            return state.chatList
+        }
+    },
+    mutations: {
+        setUser: (state, u) => {
+            state.user = u
+        },
+        setFriendlist: (state, a) => {
+            state.friendList = a
+        },
+        setChatlist: (state, a) => {
+            state.chatList = a
+        }
+    },
+    actions: {
+        asynctUser: (context, u) => {
+            context.commit('setUser', u)
+        },
+        asyncFriendlist: (context, a) => {
+            context.commit('setFriendlist', a)
+        },
+        asyncChatlist: (context, a) => {
+            context.commit('setChatlist', a)
+        }
+    }
 })
